037e85cf16cb8cc3938c2ed98f06aa56f6b88b90
A-Apkhanov <apkhanov.a.s@gmail.com>
2022-06-19 11:35:52 +0300

    Основной HTML-шаблон для компонентов приложения

---
+++ src/components/HouseComponent.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="house">
+    <div class="elevator-shaft">
+      <FloorComponent
+        v-for="fl in 10"
+        :key="'floor - ' + fl"
+      />
+      <ElevatorComponent
+        id="elevator"
+        ref="elevator"
+        class="elevator-position"
+      >
+        <IndicationComponent />
+      </ElevatorComponent>
+    </div>
+    <div class="floors">
+      <FloorComponent
+        v-for="fl in 10"
+        :key="'floor - ' + fl"
+      >
+        <template #floor-header>
+          <FloorNumberComponent />
+        </template>
+        <template #floor-content>
+          <PanelComponent />
+        </template>
+      </FloorComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+import FloorComponent from '@/components/FloorComponent.vue';
+import PanelComponent from '@/components/PanelComponent.vue';
+import FloorNumberComponent from '@/components/FloorNumberComponent.vue';
+import ElevatorComponent from '@/components/ElevatorComponent.vue';
+import IndicationComponent from '@/components/IndicationComponent.vue';
+
+export default {
+  name: 'HouseComponent',
+  components: {
+    IndicationComponent,
+    ElevatorComponent,
+    FloorNumberComponent,
+    PanelComponent,
+    FloorComponent,
+  },
+};
+</script>
+
+<style scoped>
+.house {
+  display: flex;
+  align-items: center;
+  min-height: 100vh;
+  width: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+}
+.elevator-shaft {
+  position: relative;
+  flex: 0 1 200px;
+}
+.elevator-position {
+  position: absolute;
+  bottom: 25px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.floors {
+  flex: 1 1 auto;
+}
+</style>
---
+++ src/components/PanelComponent.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="floor-panel">
+    <svg
+      data-v-049a135c
+      viewBox="0 0 1317 1024"
+      xmlns="http://www.w3.org/2000/svg"
+      width="200"
+      height="200"
+      class="direction_up active"
+    >
+      <path
+        data-v-049a135c
+        d="M0 1024 1316.571429 1024 658.285714 0 0 1024Z"
+        fill="#cdcdcd"
+      />
+    </svg>
+    <button
+      class="floor-panel_button call-button"
+    />
+    <svg
+      data-v-049a135c
+      viewBox="0 0 1317 1024"
+      xmlns="http://www.w3.org/2000/svg"
+      width="200"
+      height="200"
+      class="direction_down double"
+    >
+      <path
+        data-v-049a135c
+        d="M0 0 1316.571429 0 658.285714 1024 0 0Z"
+        fill="#cdcdcd"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PanelComponent',
+};
+</script>
+
+<style scoped>
+.floor-panel {
+  display: flex;
+  flex-direction: column;
+  width: 30px;
+  padding: 10px;
+  border-radius: 5px;
+  gap: 5px;
+  align-items: center;
+  background-color: #0a9396;
+}
+.call-button {
+  display: inline-block;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background-color: #ca6702;
+}
+.call-button:hover {
+  cursor: pointer;
+}
+.direction_up, .direction_down {
+  height: 25px;
+  width: auto;
+}
+.direction_up path, .direction_down path {
+  fill: #e9d8a6;
+}
+.active path {
+  fill: #ae2012;
+}
+.double path {
+  fill: #ff0054
+}
+</style>
